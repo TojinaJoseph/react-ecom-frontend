3cc0ee8afee4d5f6635a76434d5c272103064200
tojina <tojinajoseph123@gmail.com>
2025-05-08 15:53:19 +0530

    changes to api route

--- src/components/home/electronics/Electronics.tsx
+++ src/components/home/electronics/Electronics.tsx
@@ -12,38 +12,48 @@ export interface electronicsItems {
   rating: number;
   slug: string;
   title: string;
-
 }
 const Electronics = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const [electronics, setElectronics] = useState<electronicsItems[] | []>([]);
-  const token = localStorage.getItem('accessToken');
-  const handleNavigation=(id:number)=>{
+  const token = localStorage.getItem("accessToken");
+  const handleNavigation = (id: number) => {
     navigate(`product/${id}`);
-  }
+  };
   useEffect(() => {
-    // api.get('http://localhost:3000/products?limit=5&category=Electronics', {
-      axios.get('https://nestjs-ecom.onrender.com/products?limit=5&category=Electronics', {
-
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    axios
+      .get(
+        "https://nestjs-ecom.onrender.com/products?limit=5&category=Electronics",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
       .then(({ data }) => {
         console.log(data.data.data);
-        setElectronics(data.data.data)
+        setElectronics(data.data.data);
       })
       .catch((error) => {
-        console.error('There was an error!', error);
+        console.error("There was an error!", error);
       });
-  }, [])
+  }, []);
   return (
     <div className={`container mt-5 ${styles.electronicsContainer}`}>
-      <p>Top deals on <span className="text-primary">Electronics</span></p> <hr />
+      <p>
+        Top deals on <span className="text-primary">Electronics</span>
+      </p>{" "}
+      <hr />
       <div className="row gap-3 justify-content-center">
         {electronics.map((i) => (
-          <div className={`col-12 col-md-6 col-lg-3 col-xl-2 border text-center ${styles.box}`} key={i.id} onClick={()=>handleNavigation(i.id)}>
-            <div className={styles.img}><img src={i.featuredImageUrl} className="img-fluid"/></div>
+          <div
+            className={`col-12 col-md-6 col-lg-3 col-xl-2 border text-center ${styles.box}`}
+            key={i.id}
+            onClick={() => handleNavigation(i.id)}
+          >
+            <div className={styles.img}>
+              <img src={i.featuredImageUrl} className="img-fluid" />
+            </div>
             <div className={styles.desc}>
               <h6>{i.title}</h6>
               <p>Rs. {i.price}</p>
@@ -52,7 +62,7 @@ const Electronics = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Electronics
+export default Electronics;
